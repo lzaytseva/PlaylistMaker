c2919d5064976e3b2579bfae1a836c793cc153f9
ledrabbit <lvzaytseva1@gmail.com>
2023-03-30 18:45:57 +0300

    onClickListeners created for main activity

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -2,10 +2,35 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSearch = findViewById<Button>(R.id.btn_search)
+        val btnLibrary = findViewById<Button>(R.id.btn_library)
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
+
+        val btnSearchClickListener = object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Произошел жмяк на кнопку 'Поиск'", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnSearch.setOnClickListener(btnSearchClickListener)
+
+        btnLibrary.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Произошел жмяк на кнопку 'Медиатека'", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Произошел жмяк на кнопку 'Настройки'", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
