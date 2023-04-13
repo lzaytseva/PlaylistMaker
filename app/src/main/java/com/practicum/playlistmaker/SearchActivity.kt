f04c499120963c5c43ee8d49ae0553dabbc8c89b
ledrabbit <lvzaytseva1@gmail.com>
2023-04-13 21:59:37 +0300

    completed 2nd stage

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,74 @@
 package com.practicum.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    var savedSearchRequest = ""
+    private lateinit var searchEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<ImageView>(R.id.arrow_back)
+        val clearButton = findViewById<ImageView>(R.id.btn_clear)
+        searchEditText = findViewById(R.id.search_edit_text)
+
+        searchEditText.setText(savedSearchRequest)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        searchEditText.addTextChangedListener (object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //empty
+                savedSearchRequest = s.toString()
+                Log.e("Pup", "saved $savedSearchRequest")
+            }
+
+        })
+
+        clearButton.setOnClickListener {
+            searchEditText.setText("")
+            savedSearchRequest = ""
+        }
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    companion object {
+        const val SEARCH_ET_TEXT = "SEARCH_ET_TEXT"
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedSearchRequest = savedInstanceState.getString(SEARCH_ET_TEXT, "")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_ET_TEXT, savedSearchRequest)
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,8 +14,49 @@ class SettingsActivity: AppCompatActivity() {
 
         val btnBack = findViewById<ImageView>(R.id.arrow_back)
         btnBack.setOnClickListener {
-            val mainScreenIntent = Intent(this@SettingsActivity, MainActivity::class.java)
-            startActivity(mainScreenIntent)
+            finish()
         }
+
+        val btnShareApp = findViewById<FrameLayout>(R.id.bth_share_app)
+        btnShareApp.setOnClickListener { startShareAppActivity() }
+
+        val btnContactSupport = findViewById<FrameLayout>(R.id.bth_contact_support)
+        btnContactSupport.setOnClickListener { startContactSupportActivity() }
+
+        val btnUserAgreement = findViewById<FrameLayout>(R.id.btn_user_agreement)
+        btnUserAgreement.setOnClickListener { startOpenUserAgreementActivity() }
+    }
+
+    private fun startShareAppActivity() {
+        val message = getString(R.string.android_course_url)
+
+        val shareIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, message)
+            type = "text/plain"
+        }
+
+        startActivity(shareIntent)
+    }
+
+    private fun startContactSupportActivity() {
+        val emailTo = getString(R.string.support_email)
+        val emailSubject = getString(R.string.contact_support_email_subject)
+        val emailText = getString(R.string.contact_support_email_text)
+
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SENDTO
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(emailTo))
+            putExtra(Intent.EXTRA_SUBJECT, emailSubject)
+            putExtra(Intent.EXTRA_TEXT, emailText)
+        }
+
+        startActivity(sendIntent)
+    }
+    private fun startOpenUserAgreementActivity() {
+        val url = getString(R.string.practicum_offer)
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
     }
 }
