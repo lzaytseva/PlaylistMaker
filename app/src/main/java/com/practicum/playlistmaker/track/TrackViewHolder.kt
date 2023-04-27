a2163ec8d103dcc6676e11ba4a5e9aa9f8a2677c
ledrabbit <lvzaytseva1@gmail.com>
2023-04-27 23:07:57 +0300

    Добавлен забытый плейсхолдер и закругление углов изображения

--- app/src/main/java/com/practicum/playlistmaker/track/TrackViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/track/TrackViewHolder.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.practicum.playlistmaker.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
@@ -21,7 +22,8 @@ class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         duration.text = model.trackTime
         Glide.with(itemView)
             .load(model.artworkUrl100)
-            //.placeholder(R.drawable.barsik)
+            .placeholder(R.drawable.album_placeholder)
+            .transform(RoundedCorners(10))
             .into(albumCover)
     }
 }
