191159b7f9f2da76034b985608d9a5566783adb9
ledrabbit <lvzaytseva1@gmail.com>
2023-10-11 15:03:31 +0300

    добавлено поле isFavorite в модель Track, значение по умолчанию = false

--- app/src/main/java/com/practicum/playlistmaker/search/domain/model/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/domain/model/Track.kt
@@ -14,7 +14,8 @@ data class Track(
     val year: String,
     val primaryGenreName: String,
     val country: String,
-    val previewUrl: String
+    val previewUrl: String,
+    var isFavorite: Boolean = false
 ) : Parcelable {
 
     val artworkUrl512
